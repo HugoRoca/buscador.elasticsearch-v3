bba2b7f237ed909a628ca7bfce56bd1715a123b6
hugoroca <hugo.rock20@hotmail.com>
2020-05-24 16:40:32 -0500

    add fields order

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,60 @@
 $(document).ready(function () {
     var dataLocal = [];
     localStorage.setItem("fields", JSON.stringify(dataLocal));
+    localStorage.setItem("orders", JSON.stringify(dataLocal));
+
     $('[data-toggle="tooltip"]').tooltip();
+
     $("#add-new-field").click(function () {
         $(this).attr("disabled", "disabled");
         var index = $(".table-fields tbody tr:last-child").index();
         var row =
-            "" +
-            '<tr id="tr1">' +
-            '<td class="tg-nrix" rowspan="3">' +
-            '<select name="fields" id="fields" class="form-control">' +
-            '<option value="categorias">Categoría</option>' +
-            '<option value="textoBusqueda">Texto busqueda</option>' +
-            '<option value="marcas">Marcas</option>' +
-            '<option value="lineas">Lineas</option>' +
-            '<option value="grupoArticulos">Grupo articulos</option>' +
-            '<option value="seccion1">Sección</option>' +
-            "</select>" +
-            "</td>" +
-            "<td>" +
-            '<div class="form-check form-check-inline">' +
-            '<input class="form-check-input" type="checkbox" id="ngram" value="option1">' +
-            '<label class="form-check-label" for="ngram">ngram</label>' +
-            "</div>" +
-            "</td>" +
-            "<td>" +
-            '<input class="form-control" type="number" id="importance_ngram" min="0" max="100" value="0">' +
-            "</td>" +
-            '<td class="tg-nrix" rowspan="3">' +
-            '<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>' +
-            '<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>' +
-            "</td>" +
+            '<tr id="tr1_field">' +
+                '<td class="tg-nrix" rowspan="3">' +
+                    '<select name="fields" id="fields" class="form-control">' +
+                        '<option value="categorias">Categoría</option>' +
+                        '<option value="textoBusqueda">Texto busqueda</option>' +
+                        '<option value="marcas">Marcas</option>' +
+                        '<option value="lineas">Lineas</option>' +
+                        '<option value="grupoArticulos">Grupo articulos</option>' +
+                        '<option value="seccion1">Sección</option>' +
+                    "</select>" +
+                "</td>" +
+                "<td>" +
+                    '<div class="form-check form-check-inline">' +
+                        '<input class="form-check-input" type="checkbox" id="ngram" value="option1">' +
+                        '<label class="form-check-label" for="ngram">ngram</label>' +
+                    "</div>" +
+                "</td>" +
+                "<td>" +
+                    '<input class="form-control" type="number" id="importance_ngram" min="0" max="100" value="0">' +
+                "</td>" +
+                '<td class="tg-nrix" rowspan="3">' +
+                    '<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>' +
+                    '<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>' +
+                "</td>" +
             "</tr>" +
-            '<tr id="tr2">' +
-            "<td>" +
-            '<div class="form-check form-check-inline">' +
-            '<input class="form-check-input" type="checkbox" id="phonetic" value="option2">' +
-            '<label class="form-check-label" for="phonetic">phonetic</label>' +
-            "</div>" +
-            "</td>" +
-            "<td>" +
-            '<input class="form-control" type="number" id="importance_phonetic" min="0" max="100" value="0">' +
-            "</td>" +
+            '<tr id="tr2_field">' +
+                "<td>" +
+                    '<div class="form-check form-check-inline">' +
+                        '<input class="form-check-input" type="checkbox" id="phonetic" value="option2">' +
+                        '<label class="form-check-label" for="phonetic">phonetic</label>' +
+                    "</div>" +
+                "</td>" +
+                "<td>" +
+                    '<input class="form-control" type="number" id="importance_phonetic" min="0" max="100" value="0">' +
+                "</td>" +
             "</tr>" +
-            '<tr id="tr3">' +
-            "<td>" +
-            '<div class="form-check form-check-inline">' +
-            '<input class="form-check-input" type="checkbox" id="synonym" value="option3">' +
-            '<label class="form-check-label" for="synonym">synonym</label>' +
-            "</div>" +
-            "</td>" +
-            "<td>" +
-            '<input class="form-control" type="number" id="importance_synonym" min="0" max="100" value="0">' +
-            "</td>" +
+            '<tr id="tr3_field">' +
+                "<td>" +
+                    '<div class="form-check form-check-inline">' +
+                    '<input class="form-check-input" type="checkbox" id="synonym" value="option3">' +
+                    '<label class="form-check-label" for="synonym">synonym</label>' +
+                    "</div>" +
+                "</td>" +
+                "<td>" +
+                    '<input class="form-control" type="number" id="importance_synonym" min="0" max="100" value="0">' +
+                "</td>" +
             "</tr>";
         $(".table-fields").append(row);
         $(".table-fields tbody tr")
@@ -62,16 +64,59 @@ $(document).ready(function () {
         $('[data-toggle="tooltip"]').tooltip();
     });
 
+    $('#add-new-order').click(() => {
+        $(this).attr("disabled", "disabled");
+        var index = $(".table-orders tbody tr:last-child").index();
+        var row =
+            '<tr id="tr1_order">' +
+                '<td class="tg-nrix">' +
+                    '<select name="order" id="order_field" class="form-control">' +
+                    '<option value="ordenEstrategia">Orden estrategia</option>' +
+                    '<option value="orden">Orden personalizado</option>' +
+                    '<option value="_score">Score</option>' +
+                    "</select>" +
+                "</td>" +
+                "<td>" +
+                    '<form>'+
+                        '<div class="form-check-inline">'+
+                            '<label class="form-check-label">'+
+                                '<input type="radio" class="form-check-input" name="chkOrder" id="order_asc" checked>asc'+
+                            '</label>'+
+                        '</div>'+
+                        '<div class="form-check-inline">'+
+                            '<label class="form-check-label">'+
+                                '<input type="radio" class="form-check-input" name="chkOrder" id="order_desc">desc'+
+                            '</label>'+
+                        '</div>'+
+                    '</form>'+
+                "</td>" +
+                "<td>" +
+                    '<select name="order" id="order_importance" class="form-control">' +
+                        '<option value="alta">Alta</option>' +
+                        '<option value="media">Media</option>' +
+                        '<option value="baja">Baja</option>' +
+                    "</select>" +
+                "</td>" +
+                '<td class="tg-nrix">' +
+                    '<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>' +
+                    '<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>' +
+                "</td>" +
+            "</tr>";
+        $(".table-orders").append(row);
+        $(".table-orders tbody tr")
+            .eq(index + 1)
+            .find(".add")
+            .toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+    })
+
     $(".table-fields").on("click", ".add", function () {
         var dataLocal = localStorage.getItem("fields");
-        dataLocal =
-            dataLocal === undefined || dataLocal === null
-                ? []
-                : JSON.parse(dataLocal);
-        var field = $(this)
-            .parents("tr")
-            .find("#fields option:selected")
-            .val();
+        dataLocal = dataLocal === undefined || dataLocal === null ? [] : JSON.parse(dataLocal);
+
+        var fieldOpt = $(this).parents("tr").find("#fields option:selected");
+        var field_name = fieldOpt.text()
+        var field_value = fieldOpt.val()
         var analyzer_ngram = $('input[id="ngram"]').is(":checked");
         var analyzer_synonym = $('input[id="synonym"]').is(":checked");
         var analyzer_phonetic = $('input[id="phonetic"]').is(":checked");
@@ -83,8 +128,8 @@ $(document).ready(function () {
         if (dataLocal.length > 0) {
             for (let i = 0; i < dataLocal.length; i++) {
                 const item = dataLocal[i];
-                if (item.field === field) {
-                    alert("this field: " + field + " is already exists");
+                if (item.field === field_value) {
+                    alert("this field: " + field_name + " is already exists");
                     return false;
                 }
             }
@@ -103,7 +148,7 @@ $(document).ready(function () {
                 importance: importance_phonetic,
             });
 
-        var addField = { field, analyzers };
+        var addField = { field: field_value, analyzers };
 
         dataLocal.push(addField);
         localStorage.setItem("fields", JSON.stringify(dataLocal));
@@ -111,16 +156,69 @@ $(document).ready(function () {
         $('[data-toggle="tooltip"]').tooltip();
         var row =
             '<tr><td class="tg-nrix"><input id="id" type="hidden" value="' +
-            field +
+            field_name +
             '"/>' +
-            field +
+            field_name +
             '</td><td colspan="2">' +
             JSON.stringify(addField.analyzers) +
             '</td><td class="tg-nrix"><a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a></td></tr>';
         $(".table-fields").append(row);
         $('[data-toggle="tooltip"]').tooltip();
         $("#add-new-field").removeAttr("disabled");
-        $("#tr1,#tr2,#tr3,.tooltip").remove();
+        $("#tr1_field,#tr2_field,#tr3_field,.tooltip").remove();
+    });
+
+    $(".table-orders").on("click", ".add", function () {
+        var dataLocal = localStorage.getItem("orders");
+        dataLocal = dataLocal === undefined || dataLocal === null ? [] : JSON.parse(dataLocal);
+        var order = $(this).parents("tr").find("#order_field option:selected");
+        var order_name = order.text();
+        var order_value = order.val();
+        var order_importance = $(this).parents("tr").find("#order_importance option:selected");
+        var order_importance_name = order_importance.text();
+        var order_importance_value = order_importance.val();
+        var asc_check = $('input[id="order_asc"]').is(":checked");
+
+        if (dataLocal.length > 0) {
+            for (let i = 0; i < dataLocal.length; i++) {
+                const item = dataLocal[i];
+                if (item.order === order_value) {
+                    alert("this field: " + order_name + " is already exists");
+                    return false;
+                }
+                if (item.importance === order_importance_value) {
+                    alert("this field: " + order_importance_name + " is already exists");
+                    return false;
+                }
+            }
+        }
+
+        var object = {
+            order: order_value,
+            type: asc_check ? 'asc' : 'desc',
+            importance: order_importance_value
+        }
+
+        dataLocal.push(object);
+        localStorage.setItem("orders", JSON.stringify(dataLocal));
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        var testTypeOrder = asc_check ? 'ascendente' : 'descendente';
+        var row =
+            '<tr>'+
+                '<td><input id="id" type="hidden" value="' + order_value + '"/>' + order_name + '</td>'+
+                '<td>' + testTypeOrder + '</td>'+
+                '<td>' + order_importance_name + '</td>'+
+                '<td>'+
+                    '<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>'+
+                '</td>'+
+            '</tr>';
+        
+        $(".table-orders").append(row);
+        $('[data-toggle="tooltip"]').tooltip();
+        $("#add-new-order").removeAttr("disabled");
+        $("#tr1_order,.tooltip").remove();
     });
 
     $(".table-fields").on("click", ".delete", function () {
@@ -140,6 +238,6 @@ $(document).ready(function () {
         $(this).parents("tr").remove();
 
         $("#add-new-field").removeAttr("disabled");
-        $("#tr1,#tr2,#tr3,.tooltip").remove();
+        $("#tr1_field,#tr2_field,#tr3_field,.tooltip").remove();
     });
 });
